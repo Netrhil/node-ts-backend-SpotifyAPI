3cd6519b26b1dd4e10622d19aeab5041eb3e8b78
Jose Mansilla <Jose mansilla>
2019-04-01 19:35:43 -0300

    auth class
    Signed-off-by: Jose Mansilla <Jose mansilla>

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,10 +1,16 @@
 import { Request, Response, Router } from "express";
+import  { AuthApi, IAuthClass}  from '../services/spotify-api/auth-flow';
 
 const router = Router();
 
-router.get("/", (req: Request, res: Response) => {
+
+router.get("/", async (req: Request, res: Response) => {
+
+    const auth : IAuthClass = new AuthApi();
+    const token = await auth.getToken();
+
     res.status(200).send({
-        message: "GET request successful!!!"
+        message: "GET request successful!!!" + token
       });
 });
 
