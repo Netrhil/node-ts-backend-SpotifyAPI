dd8a7eeaf08f864773f802b97692df9a491e8d21
Jose Mansilla <Jose mansilla>
2019-04-03 23:54:35 -0300

    deleting dist
    Signed-off-by: Jose Mansilla <Jose mansilla>

--- dist/services/secrets.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.SPOTIFY_CLIENT_ID = "5de5cc1dea9a49248447e9c1fc8c883e";
-exports.SPOTIFY_CLIENT_SECRET = "f96497e6b670460a8b68279f9d9a1375";
-exports.MONGODB_ATLAS_USER = "admin";
-exports.MONGODB_ATLAS_PASS = "GenericUser";
-exports.MONGO_DB = 'falabella';
